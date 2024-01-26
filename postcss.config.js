2da311b95c9e665a74e1e86dec11a9ea5adc5005
Benjamin Preiss <benjaminpreiss@protonmail.com>
2024-01-26 16:08:36 +0100

    Add dynamic safelist to postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,22 @@
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+
+const safelist = [];
+
+setTimeout(() => {
+	safelist.push('bg-green-500');
+	console.log('pushed to safelist');
+}, '10000');
+
 export default {
-	plugins: {
-		tailwindcss: {},
-		autoprefixer: {}
-	}
+	plugins: [
+		tailwindcss({
+			safelist,
+			content: ['./src/**/*.{html,js,svelte,ts}'],
+			theme: {
+				extend: {}
+			}
+		}),
+		autoprefixer
+	]
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,8 @@
-import plugin from 'tailwindcss/plugin';
-
-const safelist = [];
-
-setTimeout(() => {
-	safelist.push('bg-green-500');
-	console.log('pushed to safelist');
-}, '10000');
-
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
 		extend: {}
 	},
-	plugins: [
-		plugin(
-			async function ({ addUtilities, addComponents, e, config }) {
-				// Add your custom styles here
-				// Watch .svelte files
-				console.log('ss');
-			},
-			{ safelist: safelist }
-		)
-	]
+	plugins: []
 };
